bf7cefefda81e7103bf1007cf6cc8bcf8ca9aab3
Rishabh Sareen <rishabh.sareen@dbtechhackathon.com>
2021-04-17 16:03:39 +0530

    adding getposts and create posts method

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from "./routes/posts.js";
+
 
 const app = express();
 app.use( bodyParser.json({ limit: "30mb", extended: true }));
 app.use( bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts",postRoutes);
 
 const URL="mongodb+srv://rishabhsareen:inazuma11@cluster0.ownp3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT=5000;
