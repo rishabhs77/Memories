50439c57078ea7d4afdb1949b118ef1747dcb6f4
Rishabh Sareen <rishabh.sareen@dbtechhackathon.com>
2021-04-18 14:17:06 +0530

    added poss and functionality to like and delete

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,25 +1,42 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
-import { createPost } from '../../actions/posts';
+import { createPost,updatePost } from '../../actions/posts';
 
-const Form = () =>{
+const Form = ({currentId, setCurrentId}) =>{
     const [postData,setPostData]=useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const dispatch=useDispatch();
+    const post= useSelector(state=>(currentId?state.posts.find((p)=>p._id===currentId):null));
+    useEffect(()=>{
+        if (post){
+            setPostData(post)
+        }
+    },[post]);
+    const clear = () =>{
+        setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
+        setCurrentId(null);
+    };
     const handleSubmit = (e)=>{
         e.preventDefault();
+        if (currentId){
+            dispatch(updatePost(currentId,postData));
+            clear();
+        }
+        else{
         dispatch(createPost(postData));
+        clear();
+        }
+        
     };
-    const clear = () =>{
-
-    };
-    const classes =useStyles();
+    
+    
+    const classes = useStyles();
     return(
         <Paper className={classes.paper}>
         <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a Memory</Typography>
+            <Typography variant="h6">{currentId?`Editing memory ${post.title}`:`Creating a memory`}</Typography>
             <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator:e.target.value })} />
             <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
             <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Post from "./Post/Post";
 import {useSelector} from 'react-redux';
+import {Grid, CircularProgress} from '@material-ui/core';
 import useStyles from './styles';
 
-const Posts = () =>{
+
+
+const Posts = ({setCurrentId}) =>{
     const classes =useStyles();
     const allPosts=useSelector( state => state.posts);
-    console.log(allPosts);
+    
+    //console.log(allPosts);
     return(
-        <div>
-            <h1>Posts</h1>
-            <Post/>
-            <Post/>
-            <Post/>
-
-        </div>
+        !allPosts.length ? <CircularProgress/>:(
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {allPosts.map( (post) => (
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                    <Post post={post} setCurrentId={setCurrentId}/>
+                </Grid>
+            ))}
+            
+        </Grid>
+        )
         
     );
 };
